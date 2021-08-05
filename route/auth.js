6d2fc691fe8d318a020dc00dc7fa6804da106476
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-08-06 02:32:42 +0300

    Update auth.js

--- route/auth.js
+++ route/auth.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const Users = require('../models/user')
 const router = Router();
 
-
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Authorization',
