762b42eda5ea666be908840c7a2ec2afde6c0bee
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-08-21 13:30:19 +0300

    cars

--- route/cars.js
+++ route/cars.js
@@ -3,18 +3,27 @@ const Cars = require('../models/cars')
 const auth = require('../middleware/auth')
 const router = Router();
 
+function isOwner(car, req) {
+    return car.userId.toString() === req.user._id.toString();
+}
+
 router.get('/', async (req, res) => {
-    const cars = await Cars.find() //getAll cars
-    // .populate('userId', 'email name') //show information about user, in second req we can get the fields that we want from users
-    // .select('title') //show only fields that we want
+    try {
+        const cars = await Cars.find() //getAll cars
+        // .populate('userId', 'email name') //show information about user, in second req we can get the fields that we want from users
+        // .select('title') //show only fields that we want
 
-    console.log(cars)
+        console.log(cars)
 
-    res.render('cars/cars', {
-        title: "Cars",
-        isCourses: true,
-        cars
-    })
+        res.render('cars/cars', {
+            title: "Cars",
+            isCourses: true,
+            userId: req.user ? req.user._id.toString() : null,
+            cars
+        })
+    } catch (err) {
+        console.log(color.bgRed.white(err))
+    }
 })
 
 router.get('/:id/edit', auth, async (req, res) => {
@@ -22,12 +31,19 @@ router.get('/:id/edit', auth, async (req, res) => {
         return res.redirect('/')
     }
 
-    const car = await Cars.findById(req.params.id)//getById(req.params.id)
+    try {
+        const car = await Cars.findById(req.params.id)//getById(req.params.id)
+        if (isOwner(car, req)) {
+            return res.redirect('/cars')
+        }
 
-    res.render('cars/car-edit', {
-        title: `Edit ${car.title}`,
-        car
-    })
+        res.render('cars/car-edit', {
+            title: `Edit ${car.title}`,
+            car
+        })
+    } catch (err) {
+        console.log(color.bgRed.white(err))
+    }
 })
 
 router.post('/edit', auth, async (req, res) => {
@@ -40,7 +56,8 @@ router.post('/edit', auth, async (req, res) => {
 router.post('/remove', auth, async (req, res) => {
     try {
         await Cars.deleteOne({
-            _id: req.body.id
+            _id: req.body.id,
+            userId: req.user._id
         })
         res.redirect('/cars')
     } catch (err) {
@@ -49,12 +66,17 @@ router.post('/remove', auth, async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const car = await Cars.findById(req.params.id)
-    res.render('cars/car', {
-        layout: 'empty',
-        title: `Car ${car.title}`,
-        car
-    })
+    try {
+        const car = await Cars.findById(req.params.id)
+        res.render('cars/car', {
+            layout: 'empty',
+            title: `Car ${car.title}`,
+            car
+        })
+    } catch (err) {
+        console.log(color.bgRed.white(err))
+    }
+
 })
 
 module.exports = router;
